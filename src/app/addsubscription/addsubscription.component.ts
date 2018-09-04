2e272f3283b5c3a36f12edc423654b04b3fff2db
mithileshtarkar <mtarkar@streetlightsoftware.com>
2018-09-04 13:50:21 +0530

    found how to work with ngmodel

---
+++ src/app/addsubscription/addsubscription.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Method {
+  id: number;
+  days: number;
+}
+
+@Component({
+  selector: 'app-addsubscription',
+  templateUrl: './addsubscription.component.html',
+  styleUrls: ['./addsubscription.component.css']
+})
+export class AddsubscriptionComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  // An array of Method
+  days: Method[] = [
+    { id: 1, days: 30 },
+    { id: 2, days: 60 }
+  ];
+
+  mySelection: number;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AddsubscriptionComponent } from './addsubscription/addsubscription.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddsubscriptionComponent
   ],
   imports: [
-    BrowserModule
+    [BrowserModule,FormsModule]
   ],
   providers: [],
   bootstrap: [AppComponent]
